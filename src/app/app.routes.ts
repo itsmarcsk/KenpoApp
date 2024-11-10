d904f6b9aea3d4fd98618db1730194fc0cd0da67
Mark <marcgimdi@gmail.com>
2024-11-10 18:12:50 +0100

    Diseño login creado

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { RegisterComponent } from './register/register.component';
+import { AppComponent } from './app.component';
+import { PaginanoencontradaComponent } from './paginanoencontrada/paginanoencontrada.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+
+export const routes: Routes = [
+    {
+        path:'login',
+        component: LoginComponent
+    },{
+        path:'register',
+        component: RegisterComponent
+    },{
+        path:'home',
+        component: HomeComponent
+    },{
+        path:'**',
+        component: PaginanoencontradaComponent
+    }
+];
